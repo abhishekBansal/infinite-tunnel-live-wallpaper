589ce5e749a402efee9b2fe69a7f52e1d377f5a5
Abhishek Bansal <discover.ab@gmail.com>
2014-12-21 20:37:37 +0530

    + fixing shared preferences crash

--- app/src/main/java/com/rrapps/infinitetunnel/Service.java
+++ app/src/main/java/com/rrapps/infinitetunnel/Service.java
@@ -1,14 +1,18 @@
 package com.rrapps.infinitetunnel;
 
-import rajawali.wallpaper.Wallpaper;
 import android.content.Context;
+
+import rajawali.wallpaper.Wallpaper;
 
 public class Service extends Wallpaper {
 	private InfiniteTunnelRenderer mRenderer;
 
 	public Engine onCreateEngine() {
 		mRenderer = new InfiniteTunnelRenderer(this);
-		return new WallpaperEngine(this.getSharedPreferences(SHARED_PREFS_NAME,
-				Context.MODE_PRIVATE), getBaseContext(), mRenderer, false);
+		return new WallpaperEngine(this.getSharedPreferences(Settings.SHARED_PREF_NAME,
+				                                            Context.MODE_PRIVATE),
+                                                            getBaseContext(),
+                                                            mRenderer,
+                                                            false);
 	}
 }
